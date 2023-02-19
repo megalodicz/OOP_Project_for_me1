9320b1dec03a98af051804028ed64d4034715309
Jarkapat <125727951+Jarkapat@users.noreply.github.com>
2023-02-19 15:34:43 +0700

    Update Page2.py

--- Page2.py
+++ Page2.py
@@ -12,3 +12,4 @@ def page2():
     st.markdown('- ขนาด 250 C.C. ได้แก่ รุ่น CRF250L, CRF250M')
     st.subheader('5. ประเภทบิ๊กไบค์ (Big Bike)')
     st.markdown(' - ขนาด 500 C.C. ได้แก่ CBR500R, CB500X, CB500F\n- ขนาด 650 cc. ได้แก่ CBR650F, CB650F\n- ขนาด 700 cc. ได้แก่ CTX700N\n - ขนาด 750 cc. ได้แก่ NC750X, Integra S, NM4\n - ขนาด 1,000 C.C. ได้แก่ CBR1000RR\n - ขนาด 1,100 cc. ได้แก่ CB1100EX\n - ขนาด 1,200 cc. ได้แก่ VFR1200F, VFR1200X Crosstourer\n - ขนาด 1,300 cc. ได้แก่ CTX1300\n - ขนาด 1,800 cc. ได้แก่ GOLDWING, GOLDWING F6B, GOLDWING F6C')
+    st.markdown('https://www.thaihonda.co.th')
